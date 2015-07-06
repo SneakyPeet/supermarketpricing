f25c36733453f01c4353df473418f7edcdf97dd8
Pieter Koornhof <pieter@entelect.co.za>
2015-07-06 20:10:32 +0200

    Add Multiple Free For Multiple Bought

--- Core/PricingModels/QuantityFreeForQuantityPricing.cs
+++ Core/PricingModels/QuantityFreeForQuantityPricing.cs
@@ -13,9 +13,33 @@ namespace Core.PricingModels
 
         public int CalculatePrice(int cost, int quantity)
         {
-            var totalWithDiscount = quantity / TotalWithDiscount();
-            var totalWithoutDiscount = quantity % TotalWithDiscount();
-            return (totalWithDiscount * quantityToBuy * cost) + (totalWithoutDiscount*cost);
+            var itemsToCalculate = quantity;
+            var price = 0;
+            while(itemsToCalculate > 0)
+            {
+                if(itemsToCalculate <= quantityToBuy)
+                {
+                    price += itemsToCalculate * cost;
+                    itemsToCalculate = 0;
+                }
+                else
+                {
+                    price += quantityToBuy * cost;
+                    itemsToCalculate -= quantityToBuy;
+                    if(itemsToCalculate > quantityFree)
+                    {
+                        itemsToCalculate -= quantityFree;
+                    }
+                    else
+                    {
+                        itemsToCalculate = 0;
+                    }
+                }
+            }
+            return price;
+            //var totalWithDiscount = quantity / TotalWithDiscount();
+            //var totalWithoutDiscount = quantity % TotalWithDiscount();
+            //return (totalWithDiscount * quantityToBuy * cost) + (totalWithoutDiscount*cost);
         }
 
         private int TotalWithDiscount()
--- Tests/ShopingCartTests.cs
+++ Tests/ShopingCartTests.cs
@@ -32,11 +32,19 @@ namespace Tests
                 yield return new TestCaseData(new List<Product> { Make3For100(), Make3For100() }, 200, "Three For A Price Should With One Item Should Cost Item Price");
                 yield return new TestCaseData(new List<Product> { Make3For100(), Make3For100(), Make3For100(), Make3For100() }, 250, "multiple Quantity For Price Should be correct");
                 yield return new TestCaseData(new List<Product> { Make3For100(), Make3For100(), Make3For100(), Make3For100(), Make3For100(), Make3For100() }, 300, "multiple Quantity For Price Should be correct");
+                
                 yield return new TestCaseData(new List<Product> { MakeBuy2OneFree() }, 100, "Buy Two Get One Free should return cost if only one is bought");
-                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree() }, 200, "Buy Two Get One Free should return correct Price");
-                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 200, "Buy Two Get One Free should return correct Price");
-                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 300, "Buy Two Get One Free should return correct Price");
-                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 500, "Buy Two Get One Free should return correct Price");
+                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree() }, 200, "Buy Two Get One Free should return correct Price: 2|2");
+                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 200, "Buy Two Get One Free should return correct Price 3|2");
+                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 300, "Buy Two Get One Free should return correct Price 4|3");
+                yield return new TestCaseData(new List<Product> { MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree(), MakeBuy2OneFree() }, 500, "Buy Two Get One Free should return correct Price 7|5");
+
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree() }, 100, "Buy Two Get Two Free should return correct Price 1|1");
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree(), MakeBuy2TwoFree() }, 200, "Buy Two Get Two Free should return correct Price 2|2");
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree() }, 200, "Buy Two Get Two Free should return correct Price 3|2");
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree() }, 200, "Buy Two Get Two Free should return correct Price 4|2");
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree() }, 300, "Buy Two Get Two Free should return correct Price 5|3");
+                yield return new TestCaseData(new List<Product> { MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree(), MakeBuy2TwoFree() }, 400, "Buy Two Get Two Free should return correct Price 7|4");
             }
         }
 
@@ -58,6 +66,11 @@ namespace Tests
         private static Product MakeBuy2OneFree()
         {
             return new Product(3, 100, new QuantityFreeForQuantityPricing(2,1));
+        }
+
+        private static Product MakeBuy2TwoFree()
+        {
+            return new Product(4, 100, new QuantityFreeForQuantityPricing(2, 2));
         }
     }
 }
